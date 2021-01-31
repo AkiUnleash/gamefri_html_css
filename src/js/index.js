20a53601cbc162d32c661dc6e31022cb870bebed
Aki Unleash <aki.unleash@gmai.comm>
2021-01-31 21:18:22 +0900

    Completion of desktop version(fine adjustment required)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-import '../scss/style.scss'
+// myself
+import '../stylesheet/style.scss'
+
+// materialize
+// import '../stylesheet/materialize.scss'
+
+// Reset CSs
+import '../stylesheet/destyle.css'
