31c7df0625c2524c6a56c524127c09f2cc796c89
Aki_Unleash <50258433+AkiUnleash@users.noreply.github.com>
2021-02-02 10:48:08 +0900

    Gam 14 top (#1)
    * Completion of desktop version(fine adjustment required)
    
    * Creating a sidebar
    
    * Header part refactoring
    
    * Section refactoring
    
    * Completion of the Top
    
    * File split
    
    * Bug fix
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-import '../scss/style.scss'
+// myself
+import '../stylesheet/style.scss'
+import '../stylesheet/header.scss'
+import '../stylesheet/footer.scss'
+
+// materialize
+// import '../stylesheet/materialize.scss'
+
+// Reset CSs
+import '../stylesheet/destyle.css'
