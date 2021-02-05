da724d0595f89a2c0c581cfde79e33c3f9074b77
Aki_Unleash <50258433+AkiUnleash@users.noreply.github.com>
2021-02-05 15:40:32 +0900

    Gam 23 profileedit (#9)
    * Gam 14 top (#1)
    
    * Completion of desktop version(fine adjustment required)
    
    * Creating a sidebar
    
    * Header part refactoring
    
    * Section refactoring
    
    * Completion of the Top
    
    * File split
    
    * Bug fix
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Completion of login screen. (#2)
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Completion of Singup screen. (#3)
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Completion of Singup finish screen. (#4)
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Completion of firebase (#5)
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Create a yml file with actions. (#6)
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Update firebase-ci.yml
    
    * Update firebase-ci.yml
    
    * Update firebase-ci.yml
    
    * Update firebase-ci.yml
    
    * Gam 53 minor correction (#8)
    
    * Add .firebase to gitignore
    
    * Minore link correction
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>
    
    * Pull Request時のActionの実行を削除
    
    * Completion of Profile edit screen.
    
    Co-authored-by: Aki Unleash <aki.unleash@gmai.comm>

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-import '../scss/style.scss'
+// myself
+import '../stylesheet/style.scss'
+import '../stylesheet/profileedit.scss'
+import '../stylesheet/singup.scss'
+import '../stylesheet/singup-finish.scss'
+import '../stylesheet/login.scss'
+import '../stylesheet/header.scss'
+import '../stylesheet/footer.scss'
+
+import '../stylesheet/mui.css'
+
+// materialize
+// import '../stylesheet/materialize.scss'
+
+// Reset CSs
+import '../stylesheet/destyle.css'
